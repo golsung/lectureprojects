c29e1090e12ec569d4e3aef604de410465db979a
golsung <golsung@naver.com>
2024-02-07 10:35:37 +0900

    Update PassOrNotWithFault.java

--- passornot-student/src/main/java/PassOrNotWithFault.java
+++ passornot-student/src/main/java/PassOrNotWithFault.java
@@ -1,8 +1,8 @@
 public class PassOrNotWithFault {
     // score1: A 과목 점수 score2: B 과목 점수
     public String ckScore(int score1, int score2) {
-        if (score1<0 ) throw new InvalidRangeException();
-        if (score1>100) throw new InvalidRangeException();
+        if (score1<0 ) throw new InvalidRangeException("negative");
+        if (score1>100) throw new InvalidRangeException("big");
         if (score1<50 || score2<50) return "과락";
         if (score1+score2>=120) return "통과";
         return "실패";
